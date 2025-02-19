b16eb79bc7f3a34da10e29e1e5c4e316a9982b0f
mt <mt@gmail.com>
2025-02-19 15:59:59 +0800

    合约部署学习作业

---
+++ script/MyTokenScript.s.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+import {MyToken} from "../src/MyToken.sol";
+
+contract MyTokenScript is Script {
+    MyToken public mytoken;
+
+    function setUp() public {}
+
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey); //开始广播交易
+        mytoken = new MyToken("MyToken", "MTK");
+        vm.stopBroadcast(); //停止广播
+
+        console.log("MyToken deployed to:", address(mytoken)); //输出合约地址
+    }
+}
